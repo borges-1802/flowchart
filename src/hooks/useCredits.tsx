807209791e6ea834c0fa86212683844437e88773
borges-1802 <jvbona98@gmail.com>
2026-07-25 22:54:48 -0300

    fix: remove useCredits

--- src/hooks/useCredits.tsx
+++
@@ -1,9 +0,0 @@
-import { useProgress } from '../contexts/ProgressContext';
-import { calculateCredits } from '../domain/creditsSummary';
-import subjectsData from '../data/subjects.json';
-import type { Subject } from '../types/subject.types';
-
-export function useCredits() {
-  const { completedIds } = useProgress();
-  return calculateCredits(subjectsData as Subject[], completedIds);
-}
