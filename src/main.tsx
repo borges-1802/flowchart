c30358b9458d25c92bf27b837930fff96ee42b9e
borges-1802 <jvbona98@gmail.com>
2026-07-29 12:38:44 -0300

    feat: ao atualizar auto F5

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
+
+const updateSW = registerSW({
+  immediate: true,
+  onRegisteredSW(_swUrl, registration) {
+    if (!registration) return
+    setInterval(() => { registration.update() }, 60 * 1000) // Checa por atualização a cada 60s enquanto a aba estiver aberta.
+  },
+  onNeedRefresh(){
+    updateSW(true)
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
